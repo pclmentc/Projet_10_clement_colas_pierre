9a24feb5b6a8e73d9a38992cfb66ace3e988e9cd
pclmentc <pclmentc@gmail.com>
2024-04-22 08:46:10 +0200

    pagination dynamique

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useEffect, useState } from "react";
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
@@ -10,15 +11,15 @@ const Slider = () => {
   const [timeoutId, setTimeoutId] = useState(null); // Garder une référence à l'ID du timeout
 
   // Tri des événements par ordre décroissant des dates
-  const byDateDesc = data?.focus.slice().sort((evtA, evtB) =>
+  const byDateDesc = data && Array.isArray(data.focus) ? data.focus.slice().sort((evtA, evtB) =>
     new Date(evtB.date) - new Date(evtA.date)
-  );
+  ) : [];
 
   // correction de l'affichage d'un slide blanc en fin de boucle
   const nextCard = () => {
-      setTimeout(() => {
+    setTimeout(() => {
       setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
-    }, 8000);
+    }, 5000);
   };
 
   const handlePaginationClick = (idx) => {
@@ -40,41 +41,37 @@ const Slider = () => {
     }
   }, [index, byDateDesc]); // Mettre à jour le timeout lorsqu'un changement d'index ou de données se produit
 
-// Vérifiez si byDateDesc est défini avant de mapper les slides
+  // Vérifiez si byDateDesc est défini avant de mapper les slides
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
-                  onClick={() => handlePaginationClick(radioIdx)}
-                />
-              ))}
-            </div>
-          </div>
-        </>
+        </div>
       ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc.map((_, radioIdx) => (
+            <input
+              key={uuidv4()}
+              type="radio"
+              name="pagination"
+              checked={index === radioIdx}
+              onClick={() => handlePaginationClick(radioIdx)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
